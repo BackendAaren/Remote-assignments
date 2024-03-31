6183c86dba6c7c977a6f6b4878efa1529c61ab27
Aaren Jhang <aaren.backend.ow95@gmail.com>
2024-03-31 19:04:24 +0800

    Add new file of Week1 Assignment

---
+++ Week-1/Assignment-3/Assignment3.js
@@ -0,0 +1,29 @@
+function countAandB(input) {
+  let count = 0;
+  for (let i = 0; i <= input.length; i++) {
+    if (input[i] === "a" || input[i] === "b") {
+      count++;
+    }
+  }
+  return count;
+}
+
+function toNumber(input) {
+  const alph = "abcdefghijklmnopqrstuvwxyz";
+  let numArr = [];
+  for (let i = 0; i < input.length; i++) {
+    numArr[i] = alph.indexOf(input[i]) + 1;
+  }
+  return numArr;
+}
+
+let input1 = ["a", "b", "c", "a", "c", "a", "c"];
+console.log(countAandB(input1)); //should print 4
+console.log(toNumber(input1)); //should print [ 1,2,3,1 ,3,1,3]
+
+let input2 = ["e", "d", "c", "d", "e"];
+console.log(countAandB(input2)); //should print 0
+console.log(toNumber(input2)); //should print [ 5, 4, 3, 4, 5 ]
+
+let input3 = ["m", "q", "w", "x", "z"];
+console.log(toNumber(input3)); //should print [ 13, 17, 23, 24, 26 ]
